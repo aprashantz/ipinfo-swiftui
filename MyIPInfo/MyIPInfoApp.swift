fe3a778f3dfd949fc29e4973c4e20b0f191e24ec
aprashantz <prashantghi8@gmail.com>
2023-10-05 23:54:13 +0545

    Initial Commit

---
+++ MyIPInfo/MyIPInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyIPInfoApp.swift
+//  MyIPInfo
+//
+//  Created by air on 05/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyIPInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
