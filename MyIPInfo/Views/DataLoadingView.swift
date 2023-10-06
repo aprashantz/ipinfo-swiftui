8bc2c442ec345a1c68d7f19cfc82a30c7e9482ec
aprashantz <prashantghi8@gmail.com>
2023-10-06 15:48:51 +0545

    add: data loading view

---
+++ MyIPInfo/Views/DataLoadingView.swift
@@ -0,0 +1,13 @@
+import Foundation
+import SwiftUI
+
+struct DataLoadingView: View {
+    var body: some View {
+        ProgressView().scaleEffect(2)
+    }
+}
+
+
+#Preview{
+    DataLoadingView()
+}
